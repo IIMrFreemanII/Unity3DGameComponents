bd2cc19baab1ee3c02de984ee78a913d56922ef8
IIMrFreemanII <kdeagovec@gmail.com>
2020-05-13 22:11:02 +0300

    add RaycastUtil.cs with MouseToHitPosition method

---
+++ Assets/GameComponents/Utils/RaycastUtil.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GameComponents.Utils
+{
+    public static class RayCastUtil
+    {
+        /// <summary>
+        /// if hit some collider return the hit position otherwise return the normalized direction of the ray movement.
+        /// </summary>
+        /// <param name="cam"></param>
+        /// <param name="mousePos"></param>
+        /// <returns></returns>
+        public static Vector3 MouseToHitPos(Camera cam, Vector3 mousePos)
+        {
+            Ray ray = cam.ScreenPointToRay(mousePos);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                return hit.point;
+            }
+
+            return ray.direction;
+        }
+    }
+}
